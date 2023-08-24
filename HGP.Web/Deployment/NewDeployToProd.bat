c740a1aaecac24009878a3bfd01d8d2ac5c1bec8
Raymond <137667254+paramountsky9990@users.noreply.github.com>
2023-08-24 13:57:28 +0900

    update all

--- HGP.Web/Deployment/NewDeployToProd.bat
+++
@@ -1,1 +0,0 @@
-"c:\Program Files (x86)\AWS Tools\Deployment Tool\awsdeploy" /verbose /log c:\logs\redeploy.txt /DDeploymentPackage="..\obj\debug\package\HGP.Web.zip" CareProdDeploy.txt
--- HGP.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HGP.Web.MvcApplication" Language="C#" %>
